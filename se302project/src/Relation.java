24480eb8351c20e87df17ed9447550fbda7a8f5d
Arda Oğulcan Üzer <arda497o@gmail.com>
2022-01-07 21:30:42 +0300

    updatex2

--- se302project/src/Relation.java
+++ se302project/src/Relation.java
@@ -1,9 +1,9 @@
 class Relation {
     private TreeRelationType type;
-    private String person1;
-    private String person2;
+    private Person person1;
+    private Person person2;
 
-    Relation(TreeRelationType type, String person1, String person2) {
+    Relation(TreeRelationType type, Person person1, Person person2) {
         this.type = type;
         this.person1 = person1;
         this.person2 = person2;
@@ -17,19 +17,19 @@ class Relation {
         this.type = type;
     }
 
-    public String getPerson1() {
+    public Person getPerson1() {
         return person1;
     }
 
-    public void setPerson1(String person1) {
+    public void setPerson1(Person person1) {
         this.person1 = person1;
     }
 
-    public String getPerson2() {
+    public Person getPerson2() {
         return person2;
     }
 
-    public void setPerson2(String person2) {
+    public void setPerson2(Person person2) {
         this.person2 = person2;
     }
 }
@@ -46,11 +46,3 @@ enum RelationType {
     FATHER, MOTHER, BROTHER, SISTER, SON, DAUGHTER, COUSIN, GRANDMOTHER, GRANDFATHER, GRANDSON, GRANDDAUGHTER, AUNT, UNCLE, HUSBAND, WIFE
 }
 
-class InvalidInputException extends Exception {
-    static final long serialVersionUID = -3387516993334229948L;
-
-    public InvalidInputException(String message) {
-        super(message);
-    }
-
-}
