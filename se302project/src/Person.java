8fba4669534bcc8cfa34e9b06cb4fce6b4eaacac
Doruk <doruksayn@hotmail.com>
2022-01-07 21:21:21 +0300

    Update

--- se302project/src/Person.java
+++ se302project/src/Person.java
@@ -1,17 +1,69 @@
 public class Person {
 
 
-        String firstName;
-        String lastName;
+    private int ID, birthYear, deathYear, currentYear;
+    private String firstName, lastName, gender;
 
-        int id;
-        String gender;
-        public Person(String firstName,String lastName){
+    private Relation parents = new Relation();
+
+    //Setters and Getters
+
+    //ID
+    public int getID(){
+        return this.ID;
+    }
+    public void setID(int newID){
+        this.ID = newID;
+    }
+    //birthYear
+    public int getbirthYear(){
+        return this.birthYear;
+    }
+    public void setbirthYear(int newbirthYear){
+        this.ID = newbirthYear;
+    }
+    //deathYear
+    public int getdeathYear(){
+        return this.deathYear;
+    }
+    public void setdeathYear(int newdeathYear){
+        this.ID = newdeathYear;
+    }
+    //currentYear
+    public int getCurrentYear(){
+        return this.currentYear;
+    }
+    public void setCurrentYear(int newcurrentYear){
+        this.currentYear = newcurrentYear;
+    }
+    //firstName
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public void setFirstName(String newfirstName){
+        this.firstName = newfirstName;
+    }
+    //lastName
+    public String getLastName(){
+        return this.lastName;
+    }
+    public void setLastName(String newlastName){
+        this.lastName = newlastName;
+    }
+    //gender
+    public String getGender(){
+        return this.gender;
+    }
+    public void setGender(String newGender){
+        this.gender = newGender;
+    }
+
+    public Person(String firstName,String lastName){
             this.firstName=firstName;
             this.lastName=lastName;
-        }
-        public String toString(){
-            return firstName+ " "+ lastName;
+    }
+    public String toString(){
+            return firstName+ " " + lastName;
         }
 
 
